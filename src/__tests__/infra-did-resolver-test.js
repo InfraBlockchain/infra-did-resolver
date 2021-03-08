e7853be37cdc7b5c3e751659c16de8124db6ed47
Bezalel Lim <bezalel.dev@gmail.com>
2021-03-08 13:39:09 +0900

    tests updated (public testnet)

--- src/__tests__/infra-did-resolver-test.js
+++ src/__tests__/infra-did-resolver-test.js
@@ -1,5 +1,7 @@
 import { Resolver } from 'did-resolver'
 import { getResolver } from "../infra-did-resolver.js"
+import { Numeric } from "eosjs";
+import { Buffer } from "buffer";
 
 describe('infra-did-resolver', () => {
 
@@ -13,7 +15,13 @@ describe('infra-did-resolver', () => {
       {
         networkId: 'local',
         registryContract: 'infradidregi',
-        rpcEndpoint: 'http://localhost:8888'
+        // rpcEndpoint: 'http://localhost:8888'
+        rpcEndpoint: 'http://9aa68844df1f.ngrok.io'
+      },
+      {
+        networkId: 'vapptest1',
+        registryContract: 'fmapkumrotfc',
+        rpcEndpoint: 'https://api.testnet.eos.io'
       }
     ]
   }
@@ -28,8 +36,15 @@ describe('infra-did-resolver', () => {
 
   describe('Public-Key-based DID', () => {
     it('resolves document', async () => {
-      const did = 'did:infra:local:PUB_K1_7nxEa8qHEiy34dpuYH4yE2zRWaAoeT1gsdTnh8n5ikapZZrzjx'
+      const networkId = 'vapptest1'
+      // const pubKey = 'PUB_K1_7nxEa8qHEiy34dpuYH4yE2zRWaAoeT1gsdTnh8n5ikapZZrzjx'
+      const pubKey = 'PUB_K1_7pM9qiBuHWF6WqRSjPTMfVYKV5ZFRavK4PkUq4oFhqi9Z46mWc'
+
+      const did = `did:infra:${networkId}:${pubKey}`
       const didDoc = await didResolver.resolve(did)
+
+      const pubKeyHex = Buffer.from(Numeric.stringToPublicKey(pubKey).data).toString('hex')
+
       // console.log({didDoc})
       console.dir(didDoc, { depth: null })
       return expect(didDoc).toEqual({
@@ -40,7 +55,7 @@ describe('infra-did-resolver', () => {
             id: `${did}#controller`,
             type: 'Secp256k1VerificationKey2018',
             controller: did,
-            publicKeyHex: '037e84547231650e816a32eb5b79028e71ac7459bbcd8e81e6697ac9022e64a407'
+            publicKeyHex: pubKeyHex //'037e84547231650e816a32eb5b79028e71ac7459bbcd8e81e6697ac9022e64a407'
           }
         ],
         authentication: [
